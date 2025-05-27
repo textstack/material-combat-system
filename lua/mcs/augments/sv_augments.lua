c4e388ed6ea25a7286ca8443985df2f86261138a
textstack <46581273+textstack@users.noreply.github.com>
2025-05-26 17:29:55 -0700

    command tweaks

--- lua/mcs/augments/sv_augments.lua
+++ lua/mcs/augments/sv_augments.lua
@@ -1,44 +1,5 @@
 util.AddNetworkString("mcs_augments")
 
-concommand.Add("mcs_set_augment", function(ply, _, args)
-	if not IsValid(ply) then return end
-
-	local dmgType = MCS.DamageType(args[1])
-	if not dmgType then
-		ply:PrintMessage(HUD_PRINTCONSOLE, "Please include a valid damage type id.")
-		return
-	end
-
-	local swep = args[2]
-	local success = ply:MCS_SetAugment(dmgType.ID, swep)
-
-	local message = "Couldn't set your augment, sorry :("
-	if success then
-		if swep then
-			message = string.format("Set %s's augment to %s.", swep, string.lower(dmgType.ServerName))
-		else
-			message = string.format("Set your current weapon's augment to %s.", string.lower(dmgType.ServerName))
-		end
-	end
-
-	ply:PrintMessage(HUD_PRINTCONSOLE, message)
-end, function(cmd, arg, args)
-	local autoCompletes = {}
-
-	local cmpArg = string.Trim(args[1] or "")
-	local add = args[2] and " " .. args[2] or ""
-
-	for id, dmgType in pairs(MCS.GetDamageTypes()) do
-		if dmgType.Hidden then continue end
-
-		if string.StartsWith(id, cmpArg) then
-			table.insert(autoCompletes, string.format("%s %s%s", cmd, id, add))
-		end
-	end
-
-	return autoCompletes
-end, "Add an augment to your current weapon.", FCVAR_NONE)
-
 net.Receive("mcs_augments", function(_, ply)
 	local swep = net.ReadString()
 	if ply.MCS_SetAugments and ply.MCS_SetAugments[swep] then return end
--- lua/mcs/concmds/cl_concmds.lua
+++ lua/mcs/concmds/cl_concmds.lua
@@ -1,2 +1,1 @@
 CreateClientConVar("mcs_enabled", 1, false, true, "Whether MCS is enabled (0 == false, 1 == let server decide, 2 == true).", 0, 2)
-CreateClientConVar("mcs_armortype", "", true, true, "Your armor type for MCS.")
