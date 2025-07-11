e2edd8b3ecdd3c4009be027bae9bdfe6a540666a
MrMonoSi <113139620+MrMonoSi@users.noreply.github.com>
2025-07-11 15:32:10 -0500

    Fixed Loricate-Splitting SFX

--- lua/mcs_types/effect/loricate-splitting.lua
+++ lua/mcs_types/effect/loricate-splitting.lua
@@ -3,13 +3,13 @@ local TYPE = {}
 TYPE.Vanilla = true
 TYPE.Set = "effect"
 TYPE.ID = "loricate-splitting"
-TYPE.ServerName = "Stupid Noise"
+TYPE.ServerName = "Thump"
 TYPE.Icon = "icon16/page_white.png"
 TYPE.Color = Color(0, 250, 255)
 
 TYPE.BaseTime = 0
 TYPE.InflictChance = 1
-TYPE.InflictSound = "npc/combine_gunship/attack_start2.wav"
+TYPE.InflictSound = "npc/vort/vort_foot4.wav"
 
 TYPE.DamageTypes = {
 	["splitting"] = true
